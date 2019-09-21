702fb5f5b2b6f9fc6cc90650a550a3f6af8f3f3b
Murdo Maclachlan <murdomaclachlan@gmail.com>
2019-09-21 21:01:56 +0100

    Create __init__.py

---
+++ lachpack/__init__.py
@@ -0,0 +1,27 @@
+#classes
+from .classes import player
+
+#clear
+from .clear import clear
+
+#end
+from .end import calculateWinner
+
+#game
+from .game import gameRound
+from .game import game
+
+#searches
+from .searches import binarySearch
+
+#setup
+from .setup import setupPlayers
+
+#sorts
+
+#validate
+from .validate import subValidateStart
+from .validate import oneNumValidate
+from .validate import oneNumSubValidate
+from .validate import twoNumValidate
+from .validate import twoNumSubValidate
