ef83ead61e8a35650b131a0a7df69e64c06e4be2
Murdo Maclachlan <murdomaclachlan@gmail.com>
2019-09-29 11:49:31 +0100

    Delete runPy.js

--- js/runPy.js
+++
@@ -1,9 +0,0 @@
-$.ajax({
-  type: "POST",
-  url: "../main.py",
-  data: { param: text}
-}).done(function( o ) {
-   // do something
-});
-
-// could be used to run the python program? more likely php via apache or github's hosting
