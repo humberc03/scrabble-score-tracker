d274ddc3fa3c77b5d1ae606f9d545650f9b2d05a
Murdo Maclachlan <murdomaclachlan@gmail.com>
2019-10-05 13:39:04 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,26 +1,8 @@
+#!/usr/bin/env python3
 import time
 from termcolor import cprint
-import lachpack
 
-#sets up the players
-players = lachpack.setupPlayers()
-
-#activates the game
-lachpack.game(players)
-
-#prints the score of each player
-print("Results: ")
-for i in range(0, len(players)):
-  print("\n -", end="", flush=True)
-  cprint(players[i].name, players[i].colour, end="", flush=True)
-  print(": " + str(players[i].score))
-
-#calculates the winners
-winners, winids = lachpack.calculateWinner(players)
-
-#prints the list of winners
-print("\nWinner(s): ")
-for i in range(0, len(winners)):
-  pos = lachpack.binarySearch(players, winids[i])
-  print("\n -", end="", flush=True)
-  cprint(winners[i], players[pos].colour)
+@app.route("/")
+@app.route("/index")
+def index():
+    return render_template("index.html")
