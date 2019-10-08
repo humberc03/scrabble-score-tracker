8a62c236a02ec78284db5e9fa359755992013048
Murdo Maclachlan <murdomaclachlan@gmail.com>
2019-10-08 23:34:40 +0100

    Update main.py

--- main.py
+++ main.py
@@ -20,11 +20,31 @@ def setup():
         lachpack.doSetup(players, i)
         i += 1
     if i > x:
-        end = "we did it"
-        return render_template("doneanddone.html", end=end)
+        loop = True
+        global totalScores
+        totalScores = [0]*len(players)
+        global scoreInputs
+        global inputTypes
+        scoreInputs = []
+        inputTypes = []
+        for i in range(4):
+            if i <= len(players)-1:
+                scoreInputs.append(players[i].name)
+                inputTypes.append("number")
+            else:
+                scoreInputs.append("nil" + str(i))
+                inputTypes.append("hidden")
+        return render_template("gameRound.html", loop=loop, input1=scoreInputs[0], input2=scoreInputs[1], input3=scoreInputs[2], input4=scoreInputs[3], type1=inputTypes[0], type2=inputTypes[1], type3=inputTypes[2], type4=inputTypes[3])
     else:
         setupDone = True
         return render_template("playerSetup.html", i=i, x=str(x))
+
+@app.route("/game", methods=["POST"])
+def game():
+    while loop == True:
+        for i in range(len(players)):
+            players[i].score = request.form[players[i].name]
+        return render_template("gameRound.html", loop=loop, input1=scoreInputs[0], input2=scoreInputs[1], input3=scoreInputs[2], input4=scoreInputs[3])
 
 if __name__ == "__main__":
     players = []
