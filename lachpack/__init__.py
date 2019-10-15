ffda163ee75eb2c4ba8aaff77f195b486f45208b
Murdo Maclachlan <murdomaclachlan@gmail.com>
2019-10-15 22:16:33 +0100

    Update __init__.py

--- lachpack/__init__.py
+++ lachpack/__init__.py
@@ -3,5 +3,15 @@
 #classes
 from .classes import Players
 
+#end
+from .end import calculateWinner
+
+#searches
+from .searches import binarySearch
+from .searches import linearSearch
+
 #setup
 from .setup import doSetup
+
+#sorts
+from .sorts import bubbleSort
